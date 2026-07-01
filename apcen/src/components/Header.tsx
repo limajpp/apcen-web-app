88923faab47ec403236e0e1e2a83b942dbbdd313
limajpp <joaopedrocal90@gmail.com>
2026-07-01 15:12:35 -0300

    :adhesive_bandage: fix: exclude alt text from non-functional img and remove it from the accessibility list

--- apcen/src/components/Header.tsx
+++ apcen/src/components/Header.tsx
@@ -3,10 +3,7 @@ import headerIcon from "@/assets/HeaderIcon.svg";
 export default function Header() {
   return (
     <div className="flex flex-row justify-center items-center h-fit w-fit py-1.75 px-4.75 gap-[8.78px]">
-      <img
-        src={headerIcon}
-        alt="Logotipo do APCEN com símbolo de microrganismo estilizado."
-      />
+      <img src={headerIcon} alt="" aria-hidden="true" />
       <h1 className="font-gunter text-[28.646px] text-[#2A59A9] leading-none mt-1">
         APCEN
       </h1>
