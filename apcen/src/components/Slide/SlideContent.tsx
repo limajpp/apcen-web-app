74fcac0ea0c70d9808dd9bb84637bc33620020f7
limajpp <joaopedrocal@edu.unifor.br>
2026-07-06 17:19:44 -0300

    :lipstick: refactor: switch progressbar position with slide

--- apcen/src/components/Slide/SlideContent.tsx
+++ apcen/src/components/Slide/SlideContent.tsx
@@ -11,11 +11,11 @@ export default function SlideContent() {
     <Card className="bg-transparent ring-0 flex flex-col w-full max-w-lg gap-6 border-none shadow-none p-0">
       <SlideHeader className="flex flex-col justify-center items-start gap-2" />
       {/* Fetch image url from backend later... */}
+      <ProgressBar className="w-full h-4 rounded-[32px] bg-[#A9C2E8]" />
       <Slide
         className="relative w-full h-98 rounded-[16px] overflow-hidden shrink-0"
         imageUrl="/images/dummySlide.png"
       />
-      <ProgressBar className="w-full h-4 rounded-[32px] bg-[#A9C2E8]" />
       <SlideLabeling />
       <Button
         size="icon"
