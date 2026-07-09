786053e2f8fd03b2b382e4cb67acbb62ee020089
limajpp <joaopedrocal@edu.unifor.br>
2026-07-09 14:21:44 -0300

    :construction: feat: fetch username from the backend

--- apcen/src/components/Slide/SlideContent.tsx
+++ apcen/src/components/Slide/SlideContent.tsx
@@ -4,11 +4,17 @@ import SlideHeader from "./SlideHeader";
 import SlideLabeling from "./SlideLabeling";
 import SlideConfirmationDialog from "./SlideConfirmationDialog";
 import { Card } from "../ui/card";
+import useAuth from "@/hooks/useAuth";
 
 export default function SlideContent() {
+  const { user } = useAuth();
+
   return (
     <Card className="bg-transparent ring-0 flex flex-col w-full max-w-lg gap-6 border-none shadow-none p-0">
-      <SlideHeader className="flex flex-col justify-center items-start gap-2" />
+      <SlideHeader
+        userName={user?.username}
+        className="flex flex-col justify-center items-start gap-2"
+      />
       {/* Fetch image url from backend later... */}
       <ProgressBar className="w-full h-4 rounded-[32px] bg-[#A9C2E8]" />
       <Slide
--- apcen/src/components/Slide/SlideHeader.tsx
+++ apcen/src/components/Slide/SlideHeader.tsx
@@ -1,13 +1,13 @@
 interface SlideHeaderProps {
+  userName: string | undefined;
   className: string;
 }
 
-export default function SlideHeader({ className }: SlideHeaderProps) {
+export default function SlideHeader({ userName, className }: SlideHeaderProps) {
   return (
     <div className={className}>
-      {/* Display username by fetching it from backend later... */}
       <h2 className="text-[#2A59A9] font-clother text-2xl font-bold">
-        Olá, Usuário01!
+        {userName ? `Olá, ${userName}!` : "Olá!"}
       </h2>
       <p className="text-[#2A59A9] font-clother text-[18px]">
         Identifique as opções na lâmina:
