6111e6853979d38dd7639bf00e5511b5c163158d
limajpp <joaopedrocal@edu.unifor.br>
2026-07-13 15:51:03 -0300

    :technologist: refactor: break active slide session and results into separate components

---
+++ apcen/src/components/Slide/SlideActiveSession.tsx
@@ -0,0 +1,36 @@
+import ProgressBar from "../ProgressBar";
+import Slide from "./SlideView/Slide";
+import SlideHeader from "./SlideHeader";
+import SlideLabeling from "./SlideLabel/SlideLabeling";
+import SlideConfirmationDialog from "./SlideConfirmationDialog";
+
+import { Card } from "@/components/ui/card";
+
+import type { User } from "@/store/auth";
+
+interface ActiveSlideSessionProps {
+  user: User | null;
+}
+
+export default function ActiveSlideSession({ user }: ActiveSlideSessionProps) {
+  return (
+    <Card className="bg-transparent ring-0 flex flex-col w-full max-w-lg gap-6 border-none shadow-none p-0">
+      <SlideHeader
+        userName={user?.username}
+        className="flex flex-col justify-center items-start gap-2"
+      />
+      {/* Fetch image url from backend later... */}
+      <ProgressBar className="w-full h-4 rounded-[32px] bg-[#A9C2E8]" />
+      <Slide
+        className="relative w-full h-98 rounded-[16px] overflow-hidden shrink-0"
+        imageUrl="https://drive.google.com/thumbnail?id=1fJ0yimv5jrlDdWttnIa059xQH41oPIIo&sz=w1000"
+      />
+      <SlideLabeling />
+      <SlideConfirmationDialog
+        confirmationText="Deseja concluir o questionário?"
+        cancelButtonText="Cancelar"
+        actionButtonText="Finalizar"
+      />
+    </Card>
+  );
+}
